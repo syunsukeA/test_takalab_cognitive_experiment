8434bdb0afb15a4678043f61b2a089ca46322d4c
syunsukeA <shunpeik2@gmail.com>
2024-12-05 04:28:33 +0900

    add: providerのバージョンを指定

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = "~> 1.10.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
   cloud {
     organization = "takalab"
     
