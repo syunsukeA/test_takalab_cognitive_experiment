2f40e2bab7df94d51415f6e1461cd55775f9e873
syunsukeA <shunpeik2@gmail.com>
2024-12-05 02:18:21 +0900

    fix: provider設定の修正

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ terraform {
   required_version = "~> 1.10.1"
   required_providers {
     aws = {
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
@@ -13,4 +14,8 @@ terraform {
       name = "test_takalab_cognitive_experiment"
     }
   }
+}
+
+provider aws {
+  region = "ap-northeast-1"
 }
