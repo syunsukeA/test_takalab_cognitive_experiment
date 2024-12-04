58dabd0d299a3b9db3c7f0205a1dad1ec7cb9d15
syunsukeA <shunpeik2@gmail.com>
2024-12-05 01:03:26 +0900

    add: バージョン指定の追加

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_version = "~> 1.10.1"
+  required_providers {
+    aws = {
+      version = "~> 5.0"
+    }
+  }
   backend "remote" {
     hostname = "app.terraform.io"
     organization = "takalab"
